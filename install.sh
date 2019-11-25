6198ea6fb92424ee2d7d4ba26934d90c4361b05d
mikamo3 <kamo3proj@gmail.com>
2019-11-25 10:49:38 +0000

    fix

--- install.sh
+++ install.sh
@@ -5,12 +5,7 @@ PACMAN_COUNTRY="Japan"
 BOOT_PARTITION_SIZE="512M"
 ROOT_PARTITION_SIZE=""
 INSTALL_TARGET_PATH=
-KERNEL_VERSION=
-TARGET_LOCALES=("en_US.UTF-8 UTF-8" "ja_JP.UTF-8 UTF-8")
-MAIN_LOCALE="en_US.UTF-8"
-TIMEZONE="/Asia/Tokyo"
 HOSTNAME="manjaro-main"
-CPU_VENDOR=
 
 install_packages_for_install() {
   echo "Install Packages for install"
@@ -46,39 +41,37 @@ create_partition() {
   sgdisk -n "0::$BOOT_PARTITION_SIZE" -t "0:ef00" "$INSTALL_TARGET_PATH"
   sgdisk -n "0::$ROOT_PARTITION_SIZE" -t "0:8300" "$INSTALL_TARGET_PATH"
   mkfs.vfat -F32 "$(lsblk "$INSTALL_TARGET_PATH" -pnlo NAME | grep -E "^$INSTALL_TARGET_PATH.+" | sed -n 1p)"
-  mkfs.ext4 "$(lsblk "$INSTALL_TARGET_PATH" -pnlo NAME | grep -E "^$INSTALL_TARGET_PATH.+" | sed -n 2p)"
+  mkfs.btrfs "$(lsblk "$INSTALL_TARGET_PATH" -pnlo NAME | grep -E "^$INSTALL_TARGET_PATH.+" | sed -n 2p)"
+}
+
+make_subvolumes() {
+  echo "make subvolumes"
+  root_path="$(lsblk "$INSTALL_TARGET_PATH" -pnlo NAME | grep -E "^$INSTALL_TARGET_PATH.+" | sed -n 2p)"
+  mount "$root_path" /mnt
+  cd /mnt
+  btrfs subvolume create @
+  btrfs subvolume create @home
+  btrfs subvolume create @var
+  cd /
+  umount /mnt
 }
 
 mount_partition() {
   echo "mount partition"
   boot_path="$(lsblk "$INSTALL_TARGET_PATH" -pnlo NAME | grep -E "^$INSTALL_TARGET_PATH.+" | sed -n 1p)"
   root_path="$(lsblk "$INSTALL_TARGET_PATH" -pnlo NAME | grep -E "^$INSTALL_TARGET_PATH.+" | sed -n 2p)"
-  mount "$root_path" /mnt
+  mount -o defaults,relatime,ssd,compress=zstd,subvol=@ "$root_path" /mnt
+  mkdir /mnt/{home,var}
+  mount -o defaults,relatime,ssd,compress=zstd,subvol=@home "$root_path" /mnt/home
+  mount -o defaults,relatime,ssd,compress=zstd,subvol=@var "$root_path" /mnt/var
   mkdir -p /mnt/boot
   mount "$boot_path" /mnt/boot
-
-}
-
-select_kernel_version() {
-  echo "select kernel version"
-  KERNEL_VERSION="$(
-    pacman -Ssq '^linux[0-9]+$' \
-      | fzf --header="Select Linux Kernel version to install"
-  )"
-  [[ -z $KERNEL_VERSION ]] && {
-    echo "abort"
-    return 1
-  }
-  return 0
 }
 
 install_base_package() {
   echo "install base package"
   timedatectl set-ntp true
-  [[ $(grep -Ec "^vendor_id.*Intel" /proc/cpuinfo) != 0 ]] && CPU_VENDOR=intel
-  [[ $(grep -Ec "^vendor_id.*AMD" /proc/cpuinfo) != 0 ]] && CPU_VENDOR=amd
-  [[ -n $CPU_VENDOR ]] && micro_code="${CPU_VENDOR}-ucode"
-  pacstrap /mnt base base-devel "$KERNEL_VERSION" "${KERNEL_VERSION}-headers" "$micro_code"
+  pacstrap /mnt base ansible
 }
 
 create_fstab() {
@@ -86,81 +79,11 @@ create_fstab() {
   genfstab -U /mnt >/mnt/etc/fstab
 }
 
-set_timezone() {
-  echo "set timezone"
-  arch-chroot /mnt ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
-  arch-chroot /mnt hwclock --systohc --utc
-}
-
-set_locale() {
-  echo "set locale"
-  : >/mnt/etc/locale.gen
-  for locale in "${TARGET_LOCALES[@]}"; do
-    echo "$locale" >>/mnt/etc/locale.gen
-  done
-  arch-chroot /mnt locale-gen
-  echo "LANG=$MAIN_LOCALE" >/mnt/etc/locale.conf
-}
-
-set_hostname() {
-  echo "set hostname"
-  echo -n "input hostname:"
-  read -r host_name
-  [[ -z $host_name ]] && {
-    echo "abort"
-    return 1
-  }
-  echo "$host_name" >/mnt/etc/hostname
-  {
-    echo "127.0.0.1 $host_name"
-    echo "::1 $host_name"
-    echo "127.0.1.1 $host_name.localdomain $host_name"
-  } >/mnt/etc/hosts
-}
-
-run_mkinitcpio() {
-  echo "run mkinitcpio"
-  arch-chroot /mnt mkinitcpio -p "$KERNEL_VERSION"
-}
-
-set_rootpasswd() {
-  echo "set root password"
-  arch-chroot /mnt passwd
-}
-
-install_bootmanager() {
-  echo "install bootmanager"
-  arch-chroot /mnt bootctl --path=/boot install
-  root_uuid=$(blkid -o value "$(df --output=source /mnt | tail -1)" | head -1)
-  echo "default manjaro
-timeout 4
-editor no
-  " >/mnt/boot/loader/loader.conf
-  mkdir -p /mnt/boot/loader/entries
-
-  vmlinuz_name=$(grep -E "^ALL_kver" "/mnt/etc/mkinitcpio.d/$KERNEL_VERSION.preset" | sed 's/^.*=\"\/boot\(.*\)"/\1/')
-  image_name=$(grep -E "^default_image" "/mnt/etc/mkinitcpio.d/$KERNEL_VERSION.preset" | sed 's/^.*=\"\/boot\(.*\)"/\1/')
-
-  [[ -n $CPU_VENDOR ]] && micro_code="initrd /$CPU_VENDOR-ucode.img"
-
-  echo "title manjaro linux
-linux $vmlinuz_name
-$micro_code
-initrd $image_name
-options root=UUID=$root_uuid rw" >/mnt/boot/loader/entries/manjaro.conf
-}
-
 install_packages_for_install
 create_partition
+make_subvolumes
 mount_partition
-select_kernel_version
 install_base_package
 create_fstab
-set_timezone
-set_locale
-set_hostname
-run_mkinitcpio
-set_rootpasswd
-install_bootmanager
 
 echo "DONE"
